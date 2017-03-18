ca56f183790ba71a461e50674909b2d7f55a7dfe
HuweyII <misc@retlaw.net>
2017-03-18 16:52:07 +0000

    Added Red Yellow Green LED

--- DSL-Power-Cycle.py
+++ DSL-Power-Cycle.py
@@ -5,8 +5,20 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(4,GPIO.OUT)
-GPIO.output(4,GPIO.HIGH)
+GPIO.output(4,GPIO.HIGH) # Relay control pin
+
+#Dictionary for which pin each LED color is on
+led_gpio = {}
+led_gpio['green'] = 16
+led_gpio['yellow'] = 20
+led_gpio['red'] = 21
+
+#Setup the pins used in the dictionary above
+GPIO.setup(16,GPIO.OUT)
+GPIO.setup(20,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
 
 hosttoping = "8.8.8.8"
 sucdelay = 120 # How many seconds to wait after a successful ping
@@ -16,6 +28,16 @@ poweroffdelay = 10 # How long to stay powered off
 bootdelay = 180 # How many seconds to wait after power cycle 
 circlekdelay = 600 # How long to wait after a power cycle failed before starting over
 circlek = False
+VERBOSE = True
+
+def led(color):
+   if color == 'off':
+      for led_off in led_gpio: #Switch all LED's off
+         GPIO.output(led_gpio[led_off],GPIO.LOW)
+   else:
+      for led_off in led_gpio: #Switch all LED's off
+         GPIO.output(led_gpio[led_off],GPIO.LOW)
+      GPIO.output(led_gpio[color],GPIO.HIGH)
 
 def ping(host):
    print "Pinging " + hosttoping + " on eth0"
@@ -25,8 +47,22 @@ def ping(host):
    else:
       return False
 
+def logme(logmsg):
+   os.system("logger -i -t DSL -p local7.info \"" + logmsg + "\"" )
+
+def vprint(*args):
+    # Verbose printing happens here
+    # Called from other locations like this
+    #  vprint ("Look how awesome my verbosity is")
+    # This function is enabled by the -v switch on the CLI
+    if VERBOSE:
+        for arg in args:
+           print arg
+           logme(arg)
+
 def kickit():
    global circlek
+   led('red')
    print "Switching power off."
    GPIO.output(4,GPIO.LOW)
    print "Power is off sleeping for " + str(poweroffdelay) + " seconds before switching it back on."
@@ -37,6 +73,7 @@ def kickit():
    time.sleep(bootdelay)
    print "Pinging after reboot."
    if ping(hosttoping):
+      led('green')
       print "OK it's working again, returning to our normaly scheduled programming."
       time.sleep(sucdelay)
    else:
@@ -46,6 +83,8 @@ def cleanup():
    GPIO.output(4,GPIO.HIGH)
    GPIO.cleanup()
 
+logme ("Test log entry from inside the system while pinging " + hosttoping + " this host.") 
+led('off')
 while True:
    if circlek == True:
       print "Strange things are afoot at the Circle-K, so we're going to sleep for " + str(circlekdelay) + " and then try again."
@@ -53,14 +92,18 @@ while True:
       print "OK lets try this again."
       circlek = False
    if ping(hosttoping):
+      led('green')
       print "Yep it's pingable. So we're going to sleep for " + str(sucdelay) + " seconds before checking again."
       time.sleep(sucdelay);
+      led('off')
    else:
+      led('yellow')
       print "Nope it didn't answer. Crap!" 
       retrycount = 0
       while retrycount <= failretry:
          print "Sleeping for " + str(faildelay) + " seconds before we try pinging again."
          time.sleep(faildelay)
+         led('off')
          if ping(hosttoping):
             print "It's pingable now"
             break
@@ -71,6 +114,7 @@ while True:
                break
             else:
                retrycount += 1
+               led('yellow')
                print "It still wasn't pingable after " + str(retrycount) + " tries." 
                print "We will keep trying until we have tried " + str(failretry) + " times."
 
